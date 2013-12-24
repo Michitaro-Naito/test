5e5f25d68874f8cf7e665ca46da94a31f1d1fc4b
Michitaro Naito <m-naito@amlitek.com>
2013-12-25 05:29:15 +0900

    Edit: post-receive.php

--- post-receive.php
+++ post-receive.php
@@ -12,7 +12,7 @@ catch(Exception $e)
 //file_put_contents('github.txt', print_r($payload, TRUE), FILE_APPEND);
 
 
-if ($payload->ref === 'refs/heads/master')
+if ($payload->ref === 'refs/heads/master' || TRUE)
 {
   // path to your site deployment script
   exec('./build.sh', $output);
